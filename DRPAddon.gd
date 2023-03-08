d03f843fca702b718d3803357470bf82aed8378a
Snazzah <me@snazzah.com>
2023-03-07 19:40:50 -0600

    fix: update DRP addon with using the last details

--- DRPAddon.gd
+++ DRPAddon.gd
@@ -1,6 +1,7 @@
 extends "res://DiscordRichPresence/ModHook.gd"
 
 onready var _yomirecord_options = get_tree().get_root().get_node("ModLoader/YOMIRecord/YOMIRecordOptions")
+var _yomirecord_last_details = "Watching a replay"
 
 func _ready():
 	var recorder = get_tree().get_root().get_node("ModLoader/YOMIRecord/YOMIRecorder")
@@ -12,6 +13,7 @@ func _yomirecord_start():
 	if not _yomirecord_options or not _yomirecord_options.get_option("drp_record"): return
 	var activityHandler = get_tree().get_root().get_node("Main/DiscordRichPresenceActivityHandler")
 	if activityHandler:
+		_yomirecord_last_details = activityHandler.activity["details"]
 		activityHandler.activity["details"] = "Recording a replay"
 		activityHandler._post_activity()
 
@@ -19,5 +21,5 @@ func _yomirecord_end():
 	if not _yomirecord_options or not _yomirecord_options.get_option("drp_record"): return
 	var activityHandler = get_tree().get_root().get_node("Main/DiscordRichPresenceActivityHandler")
 	if activityHandler:
-		activityHandler.activity["details"] = "Watching a replay"
+		activityHandler.activity["details"] = _yomirecord_last_details
 		activityHandler._post_activity()
